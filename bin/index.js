5b9cf19f0461e8d07d7fdf5d6e155bed2de4dca9
ajzaradichMV <anthony@mediavine.com>
2022-11-03 15:03:50 -0500

    More site scrape fixes
    Added in some capabilities to choose which file to use.
    
    Default file output will be to the folder that the command was ran in.

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const path = require('path');
 const colors = require('ansi-colors');
 const getUrl = require('../lib/fetch');
 const loadCommands = require('../lib/commands');
@@ -19,17 +20,29 @@ const singlebar = new progressBar.MultiBar({
 
 process.removeAllListeners('warning'); // As of Node v18.0.0 fetch is included by default but is experimental. This suppresses that warning for now.
 
+// Figure out the platform being used. If true we'll set to Windows, if false we'll just assume Mac/Linux.
+let isWin = process.platform === 'win32' ? true : false;
+
 main();
 
 async function main() {
     // Validate our user input to see if the commands being used make sense.
     validation.validateUserInput(commands)
 
-    // Set the filePath.
-    const filePath = './resources/txt/redirect_list.csv'
+    // Set the output filePath. This will place the output file in the current directory that the user is in.
+    const outputFilepath = './spr_scrape_output.txt';
+
+    // Set the default input filePath.
+    let paths = [__dirname, '../resources/txt/urls.txt'];
+    let inputFilePath = path.join.apply(null, paths);
+
+    // Set the input filePath if it's something other than empty. This is a path RELATIVE to the current directory the user is in.
+    if (commands.file != '') {
+        inputFilePath = commands.file
+    }
 
     // Create a new file or overwrite an older file.
-    fs.writeFile('./resources/txt/redirect_list.csv', 'start of file', function() {
+    fs.writeFile(outputFilepath, 'start of file', function() {
         // Do nothing.
     })
 
@@ -47,10 +60,10 @@ async function main() {
         if ( commands.output === 'console' || commands.output === undefined ) {
             consoleOutput(commands.url, response);
         } else if ( commands.output === 'file' ) {
-            fs.writeFile('./resources/txt/redirect_list.csv', 'start of file', function() {
+            fs.writeFile(outputFilepath, 'start of file', function() {
 
                 console.log(colors.green('Created file successfully.'));
-                fileOutput(commands.url, response, filePath);
+                fileOutput(commands.url, response, outputFilepath);
                 console.log(colors.green('Completed writing to file.'));
 
             })
@@ -86,7 +99,7 @@ async function main() {
             if ( commands.output === 'console' || commands.output === undefined) {
                 consoleOutput( urls[x], response );
             } else if ( commands.output === 'file' ) {
-                fileOutput( urls[x], response, filePath, firstPass );
+                fileOutput( urls[x], response, outputFilepath, firstPass );
                 barOne.increment();
             }
         }
@@ -102,9 +115,13 @@ async function main() {
     if (commands.file != undefined) {
 
         // Read from our list of URLs
-        //let sites = fs.readFileSync('./resources/txt/urls.txt', 'utf-8').split('\r'); // Need to split by \r on Windows.
-        let sites = fs.readFileSync('./resources/txt/urls.txt', 'utf-8').split('\n'); // Need to split by \n on Mac.
+        let sites = []
 
+        if (isWin) {
+            sites = fs.readFileSync( inputFilePath, 'utf-8' ).split( '\r' ); // Need to split by \r on Windows.
+        } else {
+            sites = fs.readFileSync( inputFilePath, 'utf-8' ).split( '\n' ); // Need to split by \n on Mac.
+        }
 
         // Remove newline from site in array.
         sites.forEach((site, index) => {
@@ -138,7 +155,7 @@ async function main() {
                     firstPass = true;
                 }
                 const response = await getUrl.getUrl(sites[x], commands.userAgent);
-                fileOutput(sites[x].toString(), response, filePath, firstPass);
+                fileOutput(sites[x].toString(), response, outputFilepath, firstPass);
                 barOne.increment();
             }
 
@@ -196,16 +213,16 @@ function consoleOutput(singleUrl, response) {
  * @param {*} filePath The path of the file to save the data.
  * @param {*} firstPass In order to determine if the header row should be placed, the firstPass boolean is sent. Default is false.
  */
-async function fileOutput(singleUrl, response, filePath, firstPass = false) {
+async function fileOutput(singleUrl, response, outputFilepath, firstPass = false) {
 
     // If output is status, go ahead and do that.
     if (commands.status && response.error === null || commands.search === undefined && response.error === null) {
         if (firstPass === true) {
-            await fs.promises.appendFile(filePath, '\nOriginal URL, Final URL, Status\n', function() {
+            await fs.promises.appendFile(outputFilepath, '\nOriginal URL, Final URL, Status\n', function() {
                 // Do nothing.
             })
         }
-        fs.appendFileSync(filePath, singleUrl + ',' + response.fullreq.url + ',' + response.status + '\n',function() {
+        fs.appendFileSync(outputFilepath, singleUrl + ',' + response.fullreq.url + ',' + response.status + '\n',function() {
             console.log('I appended to file')
         })
     }
@@ -213,30 +230,30 @@ async function fileOutput(singleUrl, response, filePath, firstPass = false) {
     // If output is search, go ahead and do that.
     if (commands.search && response.error === null) {
         if ( firstPass === true ) {
-            fs.appendFileSync(filePath, '\nOriginal URL,', function() {
+            fs.appendFileSync(outputFilepath, '\nOriginal URL,', function() {
                 // Do Nothing
             });
             commands.search.forEach(async (term) => {
-                await fs.appendFileSync(filePath, term + ',');
+                await fs.appendFileSync(outputFilepath, term + ',');
             });
 
         }
         let searches = {};
-        fs.appendFileSync(filePath, '\n' + singleUrl + ',', function() {
+        fs.appendFileSync(outputFilepath, '\n' + singleUrl + ',', function() {
             // Do nothing.
         });
         commands.search.forEach((term) => {
             searches[term] = response.body.includes(term);
         })
         Object.keys(searches).forEach((term) => {
-            fs.appendFileSync(filePath, searches[term] + ',', function() {
+            fs.appendFileSync(outputFilepath, searches[term] + ',', function() {
                 // Do nothing.
             });
         })
     }
 
     if ( response.error != null ) {
-        fs.appendFileSync(filePath, '\n' + singleUrl + ',Fetch failed', function() {
+        fs.appendFileSync(outputFilepath, '\n' + singleUrl + ',Fetch failed', function() {
             // Do nothing.
         });
     }
